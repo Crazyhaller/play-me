9eca30510b0dcaa305f6619da9d1590e493ba089
crazyhaller <suvigyamadrid@gmail.com>
2024-10-11 21:43:54 +0530

    UPDATE: Dashboard: API & UI

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -6,6 +6,7 @@ import { YT_REGEX } from '@/app/lib/utils'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import youtubesearchapi from 'youtube-search-api'
+import { getServerSession } from 'next-auth'
 
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
@@ -70,12 +71,58 @@ export async function POST(req: NextRequest) {
 
 export async function GET(req: NextRequest) {
   const creatorId = req.nextUrl.searchParams.get('creatorId')
+  const session = await getServerSession()
+
+  // TODO: You can get rid of the DB call here
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email ?? '',
+    },
+  })
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: 'Unauthenticated',
+      },
+      {
+        status: 403,
+      }
+    )
+  }
+
+  if (!creatorId) {
+    return NextResponse.json(
+      {
+        message: 'Invalid creatorId',
+      },
+      { status: 411 }
+    )
+  }
 
   const streams = await prismaClient.stream.findMany({
     where: {
       userId: creatorId ?? '',
     },
+    include: {
+      _count: {
+        select: {
+          upvotes: true,
+        },
+      },
+      upvotes: {
+        where: {
+          userId: user.id,
+        },
+      },
+    },
   })
 
-  return NextResponse.json({ streams })
+  return NextResponse.json({
+    streams: streams.map(({ _count, ...rest }) => ({
+      ...rest,
+      upvotes: _count.upvotes,
+      haveUpvoted: rest.upvotes.length ? true : false,
+    })),
+  })
 }
---
+++ app/components/StreamView.tsx
@@ -0,0 +1,244 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Card, CardContent } from '@/components/ui/card'
+import { ChevronUp, ChevronDown, Play, Share2 } from 'lucide-react'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Navbar } from '../components/Navbar'
+import LiteYouTubeEmbed from 'react-lite-youtube-embed'
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
+import { YT_REGEX } from '../lib/utils'
+
+interface Video {
+  id: string
+  type: string
+  url: string
+  extractedId: string
+  title: string
+  smallImg: string
+  bigImg: string
+  active: boolean
+  userId: string
+  upvotes: number
+  haveUpvoted: boolean
+}
+
+const REFRESH_INTERVAL_MS = 10 * 1000
+
+export default function StreamView({ creatorId }: { creatorId: string }) {
+  const [inputUrl, setInputUrl] = useState('')
+  const [queue, setQueue] = useState<Video[]>([])
+  const [currentVideo, setCurrentVideo] = useState<Video | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  async function refreshStreams() {
+    const res = await fetch(`/api/streams/?creatorId=${creatorId}`, {
+      credentials: 'include',
+    })
+    const json = await res.json()
+    setQueue(json.streams)
+  }
+
+  useEffect(() => {
+    refreshStreams()
+    const interval = setInterval(() => {
+      refreshStreams()
+    }, REFRESH_INTERVAL_MS)
+  }, [])
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    const res = await fetch('/api/streams/', {
+      method: 'POST',
+      body: JSON.stringify({
+        creatorId,
+        url: inputUrl,
+      }),
+    })
+    setQueue([...queue, await res.json()])
+    setInputUrl('')
+    setLoading(false)
+  }
+
+  const handleVote = (id: string, isUpvote: boolean) => {
+    setQueue(
+      queue
+        .map((video) =>
+          video.id === id
+            ? {
+                ...video,
+                upvotes: isUpvote ? video.upvotes + 1 : video.upvotes - 1,
+                haveUpvoted: !video.haveUpvoted,
+              }
+            : video
+        )
+        .sort((a, b) => b.upvotes - a.upvotes)
+    )
+
+    fetch(`/api/streams/${isUpvote ? 'upvote' : 'downvote'}`, {
+      method: 'POST',
+      body: JSON.stringify({ streamId: id }),
+    })
+  }
+
+  const playNext = () => {
+    if (queue.length > 0) {
+      setCurrentVideo(queue[0])
+      setQueue(queue.slice(1))
+    }
+  }
+
+  const handleShare = () => {
+    const shareLink = `${window.location.hostname}/creator/${creatorId}`
+    navigator.clipboard.writeText(shareLink).then(
+      () => {
+        toast.success('Link copied to clipboard', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        })
+      },
+      (err) => {
+        console.log(err)
+
+        toast.error('Failed to copy link to clipboard', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        })
+      }
+    )
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen bg-[rgb(18,10,10)] text-gray-200">
+      <Navbar />
+      <div className="max-w-4xl mx-auto p-4 space-y-6 w-full">
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-bold text-white">Song Voting Queue</h1>
+          <Button
+            onClick={handleShare}
+            className="bg-purple-700 hover:bg-purple-900 text-white"
+          >
+            <Share2 className="mr-2 h-4 w-4" />
+            Share
+          </Button>
+        </div>
+
+        <form onSubmit={handleSubmit} className="space-y-2">
+          <Input
+            type="text"
+            value={inputUrl}
+            onChange={(e) => setInputUrl(e.target.value)}
+            placeholder="Enter a YouTube URL"
+            className="text-black"
+          />
+          <Button
+            type="submit"
+            disabled={loading}
+            className="bg-purple-700 hover:bg-purple-900 text-white"
+          >
+            {loading ? 'Loading...' : 'Add to Queue'}
+          </Button>
+        </form>
+
+        {inputUrl && inputUrl.match(YT_REGEX) && !loading && (
+          <Card className="bg-gray-900 border-gray-900">
+            <CardContent className="p-4">
+              <LiteYouTubeEmbed
+                id={inputUrl.split('?v=')[1]}
+                title="YouTube video"
+                noCookie={true}
+              />
+            </CardContent>
+          </Card>
+        )}
+
+        <div className="space-y-4">
+          <h2 className="text-2xl font-bold text-white">Now Playing</h2>
+          <Card className="bg-gray-900 border-gray-800">
+            <CardContent className="p-4">
+              {currentVideo ? (
+                <>
+                  <img
+                    src="/placeholder.svg?height=360&width=640"
+                    alt="Current Video"
+                    className="w-full h-72 object-cover rounded"
+                  />
+                  <p className="mt-2 text-center font-semibold text-white">
+                    {currentVideo.title}
+                  </p>
+                </>
+              ) : (
+                <p className="text-center py-8 text-gray-400">
+                  No video playing
+                </p>
+              )}
+            </CardContent>
+          </Card>
+          <Button
+            onClick={playNext}
+            className="w-full bg-purple-700 hover:bg-purple-900 text-white"
+          >
+            <Play className="mr-2 h-4 w-4" /> Play Next
+          </Button>
+        </div>
+
+        <div className="space-y-4">
+          <h2 className="text-2xl font-bold text-white">Upcoming Songs</h2>
+          {queue.map((video) => (
+            <Card key={video.id} className="bg-gray-900 border-gray-800">
+              <CardContent className="p-4 flex items-center space-x-4">
+                <img
+                  src={video.smallImg}
+                  alt={`Thumnail for ${video.title}`}
+                  className="w-38 h-20 object-cover rounded"
+                />
+                <div className="flex-grow">
+                  <h3 className="font-semibold text-white">{video.title}</h3>
+                  <div className="flex items-center space-x-2 mt-2">
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      onClick={() =>
+                        handleVote(video.id, video.haveUpvoted ? false : true)
+                      }
+                      className="flex items-center space-x-1 bg-gray-800 text-white border-gray-700 hover:bg-gray-700"
+                    >
+                      {video.haveUpvoted ? (
+                        <ChevronDown className="h-4 w-4" />
+                      ) : (
+                        <ChevronUp className="h-4 w-4" />
+                      )}
+                      <span>{video.upvotes}</span>
+                    </Button>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+      />
+    </div>
+  )
+}
